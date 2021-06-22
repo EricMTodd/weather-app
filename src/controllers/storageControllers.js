a0e28d5b9b71542795ca0ace6a4b17fe80cec0c4
Eric M. Todd <eterictodd@gmail.com>
2021-06-21 23:21:13 -0600

    Minor formatting changes to storageControllers.js.

--- src/controllers/storageControllers.js
+++ src/controllers/storageControllers.js
@@ -1,7 +1,9 @@
 const storageControllers = (() => {
 	const checkStorageStatus = (() => {
 		if (!localStorage.weatherly) {
-			localStorage.setItem('weatherly', '');
+			let storageObject = { city: 'Denver' };
+			let storageString = JSON.stringify(storageObject);
+			localStorage.setItem('weatherly', storageString);
 		}
 		console.log(localStorage);
 	})();
